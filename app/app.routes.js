da4d539f438ee9824f532375c156fac625d0b426
Kamil Fus <balzac91@gmail.com>
2016-04-02 21:59:52 +0200

    update routing

--- app/app.routes.js
+++ app/app.routes.js
@@ -9,38 +9,43 @@
     $urlRouterProvider.otherwise('/strona-glowna');
 
     $stateProvider
-      .state('dashboard', {
-        url: '/strona-glowna',
+      .state('app', {
+        url: '/',
+        abstract: true,
+        templateUrl: './app.html'
+      })
+      .state('app.dashboard', {
+        url: 'strona-glowna',
         templateUrl: './pages/dashboard/dashboard.html',
         controller: 'DashboardController',
         controllerAs: 'vm'
       })
-      .state('learning', {
-        url: '/nauka-slowek',
+      .state('app.learning', {
+        url: 'nauka-slowek',
         templateUrl: './pages/learning/learning.html',
         controller: 'LearningController',
         controllerAs: 'vm'
       })
-      .state('words', {
-        url: '/lista-slowek',
+      .state('app.words', {
+        url: 'lista-slowek',
         templateUrl: './pages/words/words.html',
         controller: 'WordsController',
         controllerAs: 'vm'
       })
-      .state('profile', {
-        url: '/profil',
+      .state('app.profile', {
+        url: 'profil',
         templateUrl: './pages/profile/profile.html',
         controller: 'ProfileController',
         controllerAs: 'vm'
       })
-      .state('settings', {
-        url: '/ustawienia',
+      .state('app.settings', {
+        url: 'ustawienia',
         templateUrl: './pages/settings/settings.html',
         controller: 'SettingsController',
         controllerAs: 'vm'
       })
-      .state('logout', {
-        url: '/wyloguj',
+      .state('app.logout', {
+        url: 'wyloguj',
         templateUrl: './pages/logout/logout.html',
         controller: 'LogoutController',
         controllerAs: 'vm'
