1f48f3412efb62fc1eefb8b843a6593ec068ad24
Kamil Fus <balzac91@gmail.com>
2016-04-06 21:31:57 +0200

    add login method

--- app/services/auth.service.js
+++ app/services/auth.service.js
@@ -5,12 +5,22 @@
     .module('app')
     .factory('authService', authService);
 
-  function authService() {
+  authService.$inject = ['$http', 'config'];
+
+  function authService($http, config) {
     return {
       login: login
     };
 
-    function login(login, password) {
+    function login(email, password) {
+      var data = {
+        email: email,
+        password: password
+      };
+
+      return $http.post(config.apiUrl + 'authorization/login.json', data)
+        .then(function () {
+        });
     }
   }
 })();
