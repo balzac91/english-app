922116fc69c31c5fbc9bf2d29540092440eee32f
Kamil Fus <balzac91@gmail.com>
2016-04-09 12:58:01 +0200

    fix page loading

--- app/app.js
+++ app/app.js
@@ -8,12 +8,16 @@
   runBlock.$inject = ['authService', '$rootScope', '$state'];
 
   function runBlock(authService, $rootScope, $state) {
-    var onStateChangeStart = $rootScope.$on('$stateChangeStart', function (event, toState) {
+    var onStateChangeStart = $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState) {
       $rootScope.pageLoading = true;
 
       if (authService.isAuthorized() && toState.name === 'login') {
         event.preventDefault();
-        $state.go('app.dashboard');
+        if (fromState.name === 'app.dashboard') {
+          $rootScope.pageLoading = false;
+        } else {
+          $state.go('app.dashboard');
+        }
       }
 
       if (!authService.isAuthorized() && toState.name !== 'login') {
